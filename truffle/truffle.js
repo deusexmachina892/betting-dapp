812bbc3885086339054869bcc0915ad3a19cc902
deusexmachina892 <deusexmachina892@gmail.com>
2018-10-19 13:53:04 -0400

    Contract Deployment

---
+++ truffle/truffle.js
@@ -0,0 +1,34 @@
+/*
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * function when declaring them. Failure to do so will cause commands to hang. ex:
+ * ```
+ * mainnet: {
+ *     provider: function() { 
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     },
+ *     network_id: '1',
+ *     gas: 4500000,
+ *     gasPrice: 10000000000,
+ *   },
+ */
+var WalletProvider = require("truffle-wallet-provider");
+const Wallet = require('ethereumjs-wallet');
+var rinkebyPrivateKey = new Buffer("30bc97ccd6bdaba4fdf6f61e15782e00a04e417cd4ba8bcde1ccffd929bc083d","hex");
+var rinkebyWallet = Wallet.fromPrivateKey(rinkebyPrivateKey);
+var rinkebyProvider = new WalletProvider(rinkebyWallet, "https://rinkeby.infura.io/v3/e4d1ccfa9968483298c503ef17ad9b7d");
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*", // Match any network id
+    },
+    rinkeby: {
+      provider: rinkebyProvider,
+      gas: 4600000,
+      network_id: 4
+    }
+  }
+};
